c671246d4f0aad9bcb05ccb67fdbd0274260823d
martins igbadia <quicksaver03@gmail.com>
2022-09-13 18:41:17 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,57 +1,14 @@
 #include "main.h"
 
-
-
 /**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
  *
- *  * jack_bauer - prints every minute of the day
- *
- *   *
- *
- *    * Return: void
- *
- *     */
+ * Return: addition of a and b
+ */
 
-
-
-void jack_bauer(void)
-
+int add(int a, int b)
 {
-
-		int h, m, i, j, k, l;
-
-
-
-			for (h = 0; h < 24; h++)
-
-					{
-
-								i = h / 10;
-
-										j = h % 10;
-
-												for (m = 0; m < 60; m++)
-
-															{
-
-																			k = m / 10;
-
-																						l = m % 10;
-
-																									_putchar('0' + i);
-
-																												_putchar('0' + j);
-
-																															_putchar(':');
-
-																																		_putchar('0' + k);
-
-																																					_putchar('0' + l);
-
-																																								_putchar('\n');
-
-																																										}
-
-													}
-
+	return (a + b);
 }
