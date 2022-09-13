77974cd52c857fb15fc1c4ceec75d591bc9989e7
martins <quicksaver03@gmail.com>
2022-09-13 10:27:38 -0700

    find

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _abs - outputs absolut value of integer input
+ *
+ *   * @n: integer to check
+ *
+ *    *
+ *
+ *     * Return: n
+ *
+ *      */
+
+
+
+
+
+int _abs(int n)
+
+{
+
+		if (n >= 0)
+
+					return (n);
+
+
+
+			else
+
+					{
+
+								n *= -1;
+
+										return (n);
+
+											}
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
+
+Security
+
+Status
+
+D
