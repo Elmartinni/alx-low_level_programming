2eef73425d5ab12432f2c16e04f2c83e74c6cfe1
martins igbadia <quicksaver03@gmail.com>
2022-10-11 18:11:58 +0100

    Task 4

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,18 @@
-#include "dog.h" 
-  
- /** 
- *free_dog - Frees dog 
- *@d: dog to be freed 
- * 
- */ 
- void free_dog(dog_t *d) 
- { 
- if (d == NULL) 
- return; 
-  
- free(d->name); 
- free(d->owner); 
- free(d); 
- }
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
