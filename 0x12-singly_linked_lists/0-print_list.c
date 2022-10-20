86e2742c835c4d19cdb8f044dd3749008769cdf7
martins igbadia <quicksaver03@gmail.com>
2022-10-20 18:06:08 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,27 @@
-#include "main.h"
+#include "lists.h"
 
 /**
-* create_array - creates an array of chars, and
-* initializes it with a specific char.
-*@size: Size of the array
-*@c: Character to insert
-*Return: NULL if size is zero or if it fails,
-*pointer to array if everything is normal.
+* print_list - prints all the elements of a list
+*
+* @h:pointer to the list
+*
+*Return: the number of nodes
 */
 
-char *create_array(unsigned int size, char c)
+size_t print_list(const list_t *h)
 {
-	char *array;
-	unsigned int index;
+const list_t *cursor = h;
+size_t count = 0;
 
-	if (size == 0)
-		return (NULL);
-	array = malloc(sizeof(char) * size);
+while (cursor != NULL)
+{
+if (cursor->str != NULL)
+printf("[%d] %s\n", cursor->len, cursor->str);
+else
+printf("[0] (nil)\n");
+count += 1;
+cursor = cursor->next;
+}
 
-	if (array == NULL)
-		return (NULL);
-
-	for (index = 0; index < size; index++)
-		array[index] = c;
-
-	return (array);
+return (count);
 }
